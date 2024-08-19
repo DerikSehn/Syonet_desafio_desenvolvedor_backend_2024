6bf04f8a204febd089ccff21bac93e3b48955acc
Derik <derikbosing@gmail.com>
2024-08-19 17:24:31 -0300

    feat(EmailSchedulerTest):added tests

--- src/test/java/com/syonet/application/scheduler/EmailSchedulerTest.java
+++ src/test/java/com/syonet/application/scheduler/EmailSchedulerTest.java
@@ -1,6 +1,15 @@
 package com.syonet.application.scheduler;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import com.syonet.application.service.EmailService;
+import com.syonet.domain.model.Newsletter;
+import com.syonet.domain.repository.NewsletterRepository;
 
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
@@ -9,10 +18,22 @@ import jakarta.inject.Inject;
 public class EmailSchedulerTest {
 
     @Inject
-    EmailScheduler emailScheduler;
+    EmailService emailService;
+
+    @Inject
+    NewsletterRepository newsletterRepository;
 
     @Test
     public void testScheduleDailyEmail() {
-        emailScheduler.scheduleDailyEmail();
+        emailService.sendDailyNewsletters();
+
+        // assert that all newsLetters were marked as sent
+
+        List<Newsletter> newsletters = newsletterRepository.findAll().list();
+
+        for (Newsletter newsletter : newsletters) {
+            assertEquals(newsletter.sent, true);
+        }
+
     }
 }
